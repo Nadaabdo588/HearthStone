62df339c3e402a67e2ebb028c0eecd1b6575a9ab
Nadaabdo588 <nadaabdo588@gmail.com>
2021-01-29 22:32:49 +0200

    .

--- src/model/cards/spells/AOESpell.java
+++
@@ -1,8 +0,0 @@
-package model.cards.spells;
-import java.util.*;
-
-import model.cards.minions.Minion; 
-
-public interface AOESpell {
-	public void performAction(ArrayList<Minion> oppField,ArrayList<Minion> curField);
-}
--- src/model/cards/spells/MinionTargetSpell.java
+++
@@ -1,8 +0,0 @@
-package model.cards.spells;
-
-import exceptions.*;
-import model.cards.minions.Minion;
-
-public interface MinionTargetSpell {
-	public void performAction(Minion m) throws InvalidTargetException;
-}
